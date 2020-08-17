bfd61d361a0f90ef6e7f6eedf62b62452dc0e104
Will York <will@ccrc.uga.edu>
2020-08-17 06:38:34 -0400

    updated portal dependencies

--- portal/js/explore.js
+++ portal/js/explore.js
@@ -1,10 +1,13 @@
 // constants
 var nodeType = {'R':'residue', 'L':'link', 'LI':'text', 'C':'canvas'};
+var greek = {'a': '&alpha;', 'b': '&beta;', 'x': '?'};
 // data variables
 var acc = [];
 var svgPath = [];
 var jsonPath = [];
 var data = [];
+var svgCanvasColor = "rgb(255,255,255)";
+var annotated = false;
 
 function populateInput(p) {
 	acc.push(p);
@@ -76,12 +79,14 @@ function clickNode() {
 	var resID = parts["resID"];
 	var txt = getResultTxt(accession, resID, rd);
 	$("#"+iDiv).html(txt);
-	if (resID != '0') {
-		var rd = data[accession].residues['_' + resID];
+	if ((resID != '0') && (resID.match(/S/) == null) ) { 
+		// the canvas itself was not clicked
+		//  and the residue is mappable to a glycoTree object
+		var rd = data[accession].residues['#' + resID];
 		// ed is enzyme data for residues[resID]
 		var ed = rd.enzymes;
 		setupResultsTable('clickTable', ed);
-	} 
+	}
 } // end of function clickNode()
 
 
@@ -184,32 +189,50 @@ function getResultTxt(accession, resID) {
 		txt += ", which has " + data[accession].residues.length + " residues</p>";
 	} else {
 		// rd is 'residue data'
-		var rd = data[accession].residues['_' + resID];	
-		if (rd.canonical_name == "unassigned") txt += " (unassigned)";
-		txt += " residue " + resID + "</p>";
+		if (resID.match(/S/) == null) {
+			var rd = data[accession].residues['#' + resID];	
+			if (rd.canonical_name == "unassigned") txt += " (unassigned)";
+			txt += " residue " + resID + "</p>";
+			
+			// combine sugar name and ring form to give proper chemcial name
+			var sName = rd.sugar_name;
+			var nacLoc = sName.indexOf('NAc');
+			var ngcLoc = sName.indexOf('NGc');
+			var nLoc = Math.max(nacLoc, ngcLoc);
+			var rf =  "<i>" + rd.ring_form + "</i>";
+			var wholeName = sName + rf;
+			if (nLoc > -1) {
+				wholeName = sName.substring(0, nLoc) + rf + sName.substring(nLoc);
+			} else {
+				
+			}
+			var anom = greek[rd.anomer]; // lookup greek letter
+			txt += "<b>" + anom + "-" + rd.absolute_configuration + "-" + wholeName + "</b>";
+			txt += " linked to residue " + rd.parent + " at site " + rd.site;
+			txt += "<br> <a href='gctMessage.html' target='message'>GlycoCT</a> index " + rd.glycoct_index;
+			txt += "<br> <a href='https://pubchem.ncbi.nlm.nih.gov/compound/" + rd.pubchem_id +
+				"' target='pubchem'> PubChem compound: " + rd.pubchem_id + "</a>";
+			txt += "<br><table id='clickTable' class='display' width='100%'></table>";
 
-		txt += rd.anomer + "-" + rd.absolute_configuration + "-" + rd.sugar_name + rd.ring_form;
-		txt += " (GlycoCT index " + rd.glycoct_index +
-		")<br> linked to residue " + rd.parent + " at site " + rd.site;
-		
-		txt += "<br><table id='clickTable' class='display' width='100%'></table>";
-
-		txt += "<br><b>Show/Hide: </b>" +
-			"<a class='toggle-vis' data-column='0'>Gene</a>" + 
-			"; <a class='toggle-vis' data-column='1'>GlyGen</a>" + 
-			"; <a class='toggle-vis' data-column='2'>UniProt</a>" + 
-			"; <a class='toggle-vis' data-column='3'>Species</a>" + 
-			"; <a class='toggle-vis' data-column='4'>Type</a>" + 
-			"; <a class='toggle-vis' data-column='5'>DNA RefSeq</a>" + 
-			"; <a class='toggle-vis' data-column='6'>Protein RefSeq</a>"; 
+			txt += "<br><b>Show/Hide: </b>" +
+				"<a class='toggle-vis' data-column='0'>Gene</a>" + 
+				"; <a class='toggle-vis' data-column='1'>GlyGen</a>" + 
+				"; <a class='toggle-vis' data-column='2'>UniProt</a>" + 
+				"; <a class='toggle-vis' data-column='3'>Species</a>" + 
+				"; <a class='toggle-vis' data-column='4'>Type</a>" + 
+				"; <a class='toggle-vis' data-column='5'>DNA RefSeq</a>" + 
+				"; <a class='toggle-vis' data-column='6'>Protein RefSeq</a>"; 
+		} else {
+			txt += "</p><p>&#128683; The residue you clicked cannot be mapped to a glycoTree object &#128683;</p>"
+		}
 	}
 
 	return(txt);
 } // end of  function getResultTxt()
 
 
-function highlight(accession, node, type) {
-	// node is the particular element that was clicked
+function highlight(accession, clickedNode, type) {
+	// clickedNode is the <g> element whose child was clicked
 	
 	// revert all drawn elements to their original colors
 	var drawn = getDrawnElements("all");
@@ -217,16 +240,27 @@ function highlight(accession, node, type) {
 	// remove css class "boxHighlight" from all drawn elements
 	drawn.removeClass(["boxHighlight"]);
 
-	// fade the contents of the svg canvas containing the clicked node
+	// highlight the svg canvas holding the clicked node
+	// the clickedNode is (actually) <g> whose parent is <g>, whose child[0] is <g>, whose child[0] is <rect> (canvas)
+	var cn = $(clickedNode).parent().children()[0].children[0];
+	var cnn = $(cn); // convert to jquery object
+	cn.style = "";
+	cnn.addClass("boxHighlight");
+	var thefill = $(cnn).css('fill');
+	svgCanvasColor = thefill; // global scope - used by recolorElements
+	
+	// fade the contents of the svg canvas containing the clicked clickedNode
 	drawn = getDrawnElements(accession);
 	// if the svg canvas is clicked, do not fade its contents
 	if (type != "C") recolorElements(drawn, "fade");
+	// reset cnn.style (overwritten by last line)
+	cn.style = "";
 
-	// revert the clicked node to its original color(s)
+	// revert the clicked clickedNode to its original color(s)
 	// inside <svg> - must retrieve children of clickable <g> element 
 	//   (which has an id) as DOM object then convert to jquery object
 	//     also remove any DOM class attributes from these children
-	var drawnDOM = node.children; // DOM object
+	var drawnDOM = clickedNode.children; // DOM object
 	drawn = $(drawnDOM).removeClass(); // jquery object
 	// reset fill and stroke of clicked object
 	recolorElements(drawn, "revert"); 
@@ -286,7 +320,7 @@ function setupFrames() {
 	for (var i = 0; i < s.length; i++) {
 		var w = 1.0 * s[i].getAttribute('width');
 		var h = 1.0 * s[i].getAttribute('height');
-		var id = s[i].getAttribute('id').split('_')[0];
+		var accession = s[i].getAttribute('id').split('_')[0];
 		if (v > 4) console.log("  svg[" + i + "] height: " + h + "; width: " + w );
 		canvasH +=  40 + h;
 		canvasW = Math.max(canvasW, w + 20);
@@ -294,13 +328,8 @@ function setupFrames() {
 		//   allows precise positioning !!!
 		//    can be used to annotate residues with canonical IDs
 
-		var element = document.createElementNS('http://www.w3.org/2000/svg', 'text');
-		element. setAttributeNS(null, 'x', (w/2)-40);
-		element. setAttributeNS(null, 'y', 0.96*h);
-		// element. setAttributeNS(null, 'style', "font-size:18px; stroke: none;");
-		element. setAttributeNS(null, 'class', 'svgText');
-		var txt = document.createTextNode(id);
-		element. appendChild(txt);
+		// this annotation has no ID - it cannot be clicked and it is not toggled
+		var element = formTextElement((w/2)-40, 0.96*h, 'svgText', "", accession);
 		s[i].appendChild(element);
 	}
 	
@@ -327,10 +356,117 @@ function setupFrames() {
 		console.log("  canvas -> height: " + canvasH + "; width: " + canvasW );
 		console.log("  div2 -> left: " + newLeft + "; height: " + canvasH );
 	}
+	
+	annotateResidues();
+	annotated = true;
+	toggleResidueAnnotations();
 } // end of function setupFrames()
 
-	
-	
+
+
+function toggleResidueAnnotations() {
+	var b = $('#' + ifr).contents().find('body');
+	var s = b.find("svg"); // all <svg> elements in iframe body
+	var t = s.find('text[id]');  // <text> elements with 'id' attribute
+	if (!annotated) {
+		if (v > 4) console.log("Showing annotations");
+		t.removeClass(["annotationHidden"]);
+		t.addClass(["annotationShown"]);
+	} else {
+		if (v > 4) console.log("Hiding annotations");
+		t.removeClass(["annotationShown"]);
+		t.addClass(["annotationHidden"]);
+	}
+	annotated = !annotated;
+}	
+
+
+
+function annotateResidues() {
+	var b = $('#' + ifr).contents().find('body');
+	var s = b.find("svg"); // all <svg> elements in iframe body
+
+	for (var i = 0 ; i < s.length; i++) { // for each svg image
+		var accession = s[i].id.split("_")[0];
+		if (v > 3) console.log("##### annotating glycan " + accession + " #####");
+
+		var ss = $(s[i]).removeClass(); // svg image to jquery object
+		var g = ss.find('g[id]');  // <g> elements with 'id' attribute
+
+		g.each(function( index2 ) { // each descendant <g> element having 'id'
+
+			var parts = parseID(this.id);
+			var type =  parts['type'];
+			var resID = parts['resID'];
+			if (type == "R") { // annotate residues only
+				var x = 0;
+				var y = 0;
+				var d = this.children[0]; // first child of the <g> element having 'id'
+				if (v > 4) console.log("    annotating node " + index2 + ": type " + type + "; id " + resID);
+				var nn = d.nodeName;
+				switch(nn) {
+					case "circle":
+						x = 1 * d.getAttribute('cx');
+						y = 1 * d.getAttribute('cy');
+						if (v > 4) console.log("         <circle> at " + x + "," + y);
+						break;
+					case "rect":
+						var h = 1 * d.getAttribute('height')
+						x = 1 * d.getAttribute('x') + h/2;
+						var w = 1 * d.getAttribute('width')
+						y = 1 * d.getAttribute('y') + w/2;
+						if (v > 4) console.log("         <rect> at " + x + "," + y);
+						break;
+					case "polygon":
+						var p = d.getAttribute("points").trim();
+						var pa = p.split(' ');
+						var maxEven = 0;
+						var minEven = 1000;
+						var maxOdd = 0;
+						var minOdd = 1000;
+						for (var q = 0; q < pa.length; q++) {
+							if (q%2 == 0) {
+								minEven = Math.min(minEven, pa[q]);
+								maxEven = Math.max(maxEven, pa[q]);
+							} else {
+								minOdd = Math.min(minOdd, pa[q]);
+								maxOdd = Math.max(maxOdd, pa[q]);									
+							}
+						}
+						y = (minOdd + maxOdd) / 2;
+						x = (minEven + maxEven) / 2;
+						if (v > 4) console.log("         <polygon> at " + x + "," + y);
+						break;
+				}
+				var elemID = "A-" + accession + ":" + resID;
+				var element = formTextElement(x, y, 'annotationShown', elemID, resID);
+				s[i].appendChild(element);
+			}
+
+		});
+
+	};
+
+} // end of function annotateResidues()
+
+
+function formTextElement(x, y, c, id, t) {
+	var element = document.createElementNS('http://www.w3.org/2000/svg', 'text');
+	element. setAttributeNS(null, 'x', x);
+	element. setAttributeNS(null, 'y', y);
+	element. setAttributeNS(null, 'class', c);
+	if (id != "") {
+		element. setAttributeNS(null, 'id', id);
+	}
+	var txt = document.createTextNode(t);
+	element. appendChild(txt);
+	return(element);
+} // end of function formTextElement()
+
+
+
+
+
 function getDrawnElements(accession) {
 	var b = $('#' + ifr).contents().find('body');
 	var select = "#" + accession + "_svg";
@@ -352,8 +488,10 @@ function getDrawnElements(accession) {
 	
 function recolorElements(drawn, mode) {
 	// mode 'revert' reverts back to original colors; 'fade' fades colors
+	// the following two variables have global scope - these should be changed globally
+	//   svgCanvasColor is set dynamically, e.g., by fetching the fill color of a canvas
 	var fp = fadePercent;
-	var bg = $('#' + ifr).css('background-color'); 
+	var bg = svgCanvasColor; 
 	if (v > 2) console.log("### Recoloring " + drawn.length + " Element(s) ###");
 	drawn.each(function( index ) {
 		if (v > 4) {
@@ -458,8 +596,8 @@ function addSVGevents() {
 		var residues = glycan.residues;
 		// resIndex cannot be 0 (-> canvas) or contain "S" (-> unmapped node)
 		if ( (resIndex != 0) && (!resIndex.includes("S") ) ) {
-			// To avoid resKey conflicts, resKey always begins with "_"
-			var resKey = "_" + resIndex;
+			// To avoid resKey conflicts, resKey always begins with "#"
+			var resKey = "#" + resIndex;
 			var res = residues[resKey]; 
 
 			if (v > 4) {
@@ -475,18 +613,18 @@ function addSVGevents() {
 } // end of function addSVGevents()
 
 function setResidueKeys() {
-	// converts residues of each glycan from array[0,1,2,...] to associative array
-	if (v > 1) console.log("#### Setting Up Associative Arrays for Residues ####");
+	// converts residues of each glycan from array[val0,val1,val2,...] to Object {'key': 'value'}
+	if (v > 1) console.log("##### Assigning Keys to Residues #####");
 	for (var key in data) {
 		var glycan = "glycan[" + key + "]";
 		var residues = data[key].residues;
 		for (var j = 0; j < residues.length; j++ ) {
 			//console.log("j is " + j);
-			// FAILS WHEN resID == j - so need prefix "_"
-			var key2 = "_" + residues[j].residue_id;
+			// FAILS WHEN resID == j - so need prefix "#"
+			var key2 = "#" + residues[j].residue_id;
 			residues[key2] = residues[j];
 			if (v > 4) {
-				console.log("   associative key generated for " + glycan + ".residues[" + j + "] -> '" +
+				console.log("   object key generated for " + glycan + ".residues[" + j + "] -> '" +
 							key2 + "'");
 				console.log("   data['" + key + "'].residues['" + key2 + "'].canonical_name is " + 
 							data[key].residues[key2].canonical_name);
@@ -501,48 +639,20 @@ function showData() {
 	// show json data
 	// TODO: make txt depend on v
 	var txt = "<pre>";
+	
+	var count = 0;
+	var sep = "";
 	for (var key in data) {
 		var glycan = data[key];
-		txt += "\nglycan: " + key + "";
-		var residues = glycan.residues;
-		for (var j = 0; j < residues.length; j++ ) {
-			var res =residues[j];  // reused for all v > 1
-			if (v > 1) {
-				var resID = res.residue_id;
-				var canon = res.canonical_name;
-				txt += "\n  residue: " + resID + "; canonical_name: " + canon + ";";
-				txt += " linked to  " + res.site + "' of " + res.parent;
-			}
-			if (v > 2) {
-				//var anomer = res.anomer;
-				txt += "\n    anomer: " + res.anomer;
-				txt += "; absolute_configuration: " + res.absolute_configuration;
-				txt += "; sugar_name: " + res.sugar_name;
-				txt += "; ring_form: " + res.ring_form;
-				txt += "; parent: " + res.parent;
-				txt += "; linkage site: " + res.site;
-				txt += ";";
-			}
-			if (v > 3) {
-				var enzymes = res.enzymes;
-				txt += "\n    enzymes:"
-				for (var k = 0; k < enzymes.length; k++) {
-					enz = enzymes[k];
-					txt += "\n      gene_name: " + enz.gene_name;
-					txt += "; species: " + enz.species;
-					txt += "; type: " + enz.type;
-					txt += "; uniprot: " + enz.uniprot;
-					txt += "\n        orthology_group: " + enz.orthology_group;
-					txt += "; gene_id: " + enz.gene_id;
-					txt += "; dna_refseq: " + enz.dna_refseq;
-					txt += "; protein_refseq: " + enz.protein_refseq;
-				}
-			}
-		}
+		var total = JSON.stringify(glycan, undefined, 2);
+		if (count++ > 0) sep = ",\n\n";
+		txt += sep + total;
 	}
 	txt += "</pre>";
 	$('#' + iDiv).html(txt);
 }  // end of function showData()
+
+
 
 function setupCSSiframe(ifrID, cssSrc) {
 	// The iframe cannot use css files imported into the main document
