e4384747627523a43b50a8ab1a3a5074ae2fdb60
Will York <will@ccrc.uga.edu>
2021-04-03 15:22:24 -0400

    incremental improvements to pathway visualizer

--- portal/js/renderPaths.js
+++ portal/js/renderPaths.js
@@ -1,5 +1,7 @@
 function initialize() { 
 	// TODO: convert all d3 style attributes to css
+	
+	var globalData;
 	var dataPath = "api/paths/getpathD3v3.php";
 	var arg = window.location.search.substring(1);
 	var a = arg.split("&");
@@ -12,13 +14,17 @@ function initialize() {
 	var mouse_hl = '#25c';
 	
 	var helpMessage = "<p>Mouse Over or Click on a Graphical Element</p>";
-
+	var subURL = "https://gnome.glyomics.org/restrictions/GlyGen.StructureBrowser.html?focus";
 	// as arcScale increases, horizontal size of arc dereases
 	var arcScale = 10;
 	var margin = {top: 20, right: 20, bottom: 20, left: 20};
 	var width = 600 - margin.left - margin.right;
 	var height = 3000 - margin.top - margin.bottom;
 	var node_x = 300;
+	var label_x = 50;
+	var logo_x = 110;
+	var sand_x = 135;
+	var sub_x = 175;
 
 	// append the svg object to the body of the page
 	var svg = d3.select("#pathgraph")
@@ -48,6 +54,9 @@ function initialize() {
 	}
 
 	function showReaction(d) {
+		// build html for visualizing a reaction and related data and links
+		// maybe good to separate content (txt) and destination (rxnWindow)
+		//    less advantageous: add destination as an argument
 		var txt = "<center><h3>Reaction Details Page</h3></center>";
 		txt += "<p>This page will contain a graphical representation of extensive details for the reaction <br> " + d.source + " &rarr;  " + d.target + "</p>";
 		txt += "<p>This will include svg images of the glycans along with information about the " + d.enzymes.length + " (GT or GH) enzymes that directly affect <b>residue " + d.residue_added + "</b>, which is added or removed during the reaction.</p><p>This requires additional queries to the database API.</p>";
@@ -60,13 +69,25 @@ function initialize() {
 		rxnWindow.focus();
 	}
 
-
+	function showGlycan(d) {
+		// implemented outside of D3, no direct access to 'data', us globalData
+		// alert(globalData.path_count);
+		var rs = $("#results");
+		$.get("svg/" + d.name + ".gTree.svg", function( response ) {
+			// TODO: save all these as subelements of data upon loading to reduce latency
+			var result = (new XMLSerializer()).serializeToString(response);
+			rs.html("<center><h1>Glycan " + d.name + "</h1></center><br>" + result);
+		});
+	}
 	
-	// process data from API
+	// USE D3 to process data from API 
 	d3.json(theURL, function( data) {
+		// provide access to data to functions outside of D3
+		globalData = data;
+		
 		document.getElementById("headerDiv").innerHTML = "<h2>Pathways from " + a[1] + " to " + a[0] + "</h2>"; 
 
-				// map each node to its id so given link source or target, node can be retrieved
+		// map each node to its id so given link source or target, node can be retrieved
 		var id_to_node = {};
 		data.nodes.forEach(function (n) {
 			id_to_node[n.id] = n;
@@ -75,35 +96,66 @@ function initialize() {
 		// path_count value
 		var pc = data.path_count;
 
-		// d.cc holds the node color - corresponds to dp
-		//  d.y specifies the vertical position of each node
+		// add drawing variables to each node in data.nodes
 		var cc;
 		var sh;
 		var nodeCount = 0;
 		data.nodes.forEach(function( d, i ) {
 			nodeCount++;
-			d.cc = pal[d.dp];
-			d.sh = shading[nodeCount % 2];
-			d.y = vSpacing * nodeCount;
+			d.cc = pal[d.dp]; // color of the node, depends on dp
+			d.sh = shading[nodeCount % 2]; // shading of the node's 'strip'
+			d.y = vSpacing * nodeCount; // vertical spacing of nodes
 			d.url = "https://www.glygen.org/glycan/" + d.name;
 			d.isSelected = 0;
 		});
 
-		// calculate parameters for arcs
+		// add drawing parameters for arcs
 		var edgeCount = 0;
-		var maxSpan = 1;
+		var maxSpan = 1; // the maximum vertical distance spanned by an arc
 		data.links.forEach(function (d) {
 			edgeCount++;
 			var soy = id_to_node[d.source].y 
 			var tay = id_to_node[d.target].y
 			maxSpan = Math.max(maxSpan, Math.abs(tay - soy));
-			// arcScale defines how 'tall' each arc is
+			// arcScale defines how 'wide' each arc is
 			arcScale = Math.min(20, 1800 / maxSpan);
 		});
 
 		document.getElementById("headerDiv").innerHTML += "Number of nodes: " +  nodeCount + "; Number of links: " +  edgeCount + "; Number of unique paths: " + pc;
 
+		function nodeOver(d) {
+			showGlycan(d);
+			// Highlight the connections to/from the node
+			links
+				.classed('arcHot', function (link_d) {
+				  return link_d.source === d.id || link_d.target === d.id ? true : false;					
+				})
+				.classed('arcPale', function (link_d) {
+				  return link_d.source !== d.id && link_d.target !== d.id ? true : false;					
+				})
 
+			labels
+				.classed('labelHot', function (label_d) {
+					return label_d.id === d.id ? true : false;
+				})
+			dpLabels
+				.classed('labelHot', function (label_d) {
+					return label_d.id === d.id ? true : false;
+				})
+		}
+		
+		function nodeOut(d) {
+			$("#results").html("");
+			links
+				.classed('arcHot', false)
+				.classed('arcPale', false);
+			labels
+				.classed('labelHot', false);
+			dpLabels
+				.classed('labelHot', false);
+		}
+		
+		
 
 		// draw alternate shading first (background)
 		var shade = svg
@@ -160,19 +212,18 @@ function initialize() {
 				conciseReaction(d);
 				links
 					.classed('arcHot', function (link_d) {
-					  return link_d.source === d.source && link_d.target === d.target ? true : false;					
-				  })				
+					return link_d.source === d.source && link_d.target === d.target ? true : false;					
+				})	
+				.classed('arcPale', function (link_d) {
+				  return link_d.source !== d.id && link_d.target !== d.id ? true : false;					
+				})				
 			})
 			.on('mouseout', function (d) {
 				$("#results").html("");
-				links
-					.classed('arcHot', false);	
+				nodeOut(d);
 			})
 			.on("click", function(d) {
 				showReaction(d);
-				/*
-
-				*/
 			});
 
 		// draw nodes
@@ -188,10 +239,16 @@ function initialize() {
 			// cannot use css for fill, as (d.cc) depends on the dp
 			.style("fill", function(d){ return(d.cc)})
 			.on("click", function(d) {
-				// window.open("https://www.glygen.org/glycan/" + d.name);
 				d.isSelected = 1;
 				showNode(d);
 			})
+			.on('mouseover', function (d) {
+				nodeOver(d);
+			})
+			.on('mouseout', function (d) {
+				nodeOut(d);
+			})
+		
 		
 		// label dps	
 		var dpLabels = svg
@@ -210,54 +267,119 @@ function initialize() {
 			.data(data.nodes)
 			.enter()
 			.append("text")
-			.attr("x", 50)
+			.attr("x", label_x)
 			.attr("y", function(d){ return(d.y+5)})
 			.classed('labelDefault', true)
 			.text(function(d){ return(d.name)})
+			.on('mouseover', function (d) {
+				nodeOver(d);
+			})
+			.on('mouseout', function (d) {
+				nodeOut(d);
+			})
+			.on("click", function(d) {
+				d.isSelected = 1;
+				showNode(d);
+			})	
+
+		var sandboxes = svg
+			.selectAll()
+			.data(data.nodes)
+			.enter()
+			.append('g')
 			.on("click", function(d) { 
 				window.open("explore.html?" + d.name); 
 			})
-			.on('mouseover', function (d) {
-				$("#results").load("svg/" + d.name + ".gTree.svg");
-				labels
-					.classed('labelHot', function (label_d) {
-						return label_d.id === d.id ? true : false;
-					})
-			})
-			.on('mouseout', function (d) {
-				$("#results").html("");
-				labels
-					.classed('labelHot', false);
-			});	
+		
+		// separately append graphic elements to 'sandboxes'
+		sandboxes.append('rect')
+			.attr("x", sand_x)
+			.attr("y", function(d){ return(d.y-9)})
+			.attr("width", 20)
+			.attr("height", 18)
+			.style("fill", function(d){ return(d.sh)})
+			.classed('linkbox', true)
 
-		 // Add the mouseover functionality 
-		nodes
-			.on('mouseover', function (d) {
-				$("#results").load("svg/" + d.name + ".gTree.svg");
-				// Highlight the connections to/from the node
-				links
-					.classed('arcHot', function (link_d) {
-					  return link_d.source === d.id || link_d.target === d.id ? true : false;					
-				  })
+		sandboxes.append('path')
+			.attr('d', function (d) {
+				start = d.y
+				return [
+					'M',sand_x,start,'l',15.20,4.40,'l',4.00,-5.60,
+					'm',-19.20,1.20,'l',4.40,-5.60,'l',14.80,4.40,
+					'm',-4.00,8.40,'l',-15.20,-4.40
+				]
+				.join(' ')
+			})
+			.classed('logo', true)
 	
-				labels
-					.classed('labelHot', function (label_d) {
-						return label_d.id === d.id ? true : false;
-					})
-				dpLabels
-					.classed('labelHot', function (label_d) {
-						return label_d.id === d.id ? true : false;
-					})
+
+		
+		var logos = svg
+			.selectAll()
+			.data(data.nodes)
+			.enter()
+			.append("g")
+			.on("click", function(d) {
+				window.open("https://www.glygen.org/glycan/" + d.name);
 			})
-			.on('mouseout', function (d) {
-				$("#results").html("");
-				links
-					.classed('arcHot', false);
-				labels
-					.classed('labelHot', false);
-				dpLabels
-					.classed('labelHot', false);
+		
+		logos.append('rect')
+			.attr("x", logo_x - 7)
+			.attr("y", function(d){ return(d.y-9)})
+			.attr("width", 25)
+			.attr("height", 18)
+			.style("fill", function(d){ return(d.sh)})
+			.classed('linkbox', true)
+		
+		logos.append('path')
+			.attr('d', function (d) {
+				start = d.y - 4
+				return [
+					'M',logo_x,start,'l',12.65,3.65,'l',4.2,-4.85,	
+					'm',-24.5,10.35,'l',4,-4.65,'l',12.6,3.8,
+					'm',1.85,-2.3,'l',-12.7,-3.7
+				]
+				.join(' ')
 			})
+			.classed('logo', true)
+
+		
+		var subbrows = svg
+			.selectAll()
+			.data(data.nodes)
+			.enter()
+			.append("g")
+			.on("click", function(d) {
+				window.open(subURL + "=" + d.name);
+			})
+
+		
+		subbrows.append('rect')
+			.attr("x", sub_x - 10)
+			.attr("y", function(d){ return(d.y-9)})
+			.attr("width", 20)
+			.attr("height", 18)
+			.style("fill", function(d){ return(d.sh)})
+			.classed('linkbox', true)
+		
+		subbrows.append('path')
+			.attr('d', function (d) {
+				start = d.y - 7.5
+				return [
+					'M',sub_x,start,'a',1.8,1.8,1,1,0,0.001,0,
+					'm',0,3.6,'l',0,2.7,
+					'a',1.8,1.8,1,1,0,0.001,0,
+					'm',1.35,2.700,'l',2.7,2.7,
+					'm',1.8,0,'a',1.8,1.8,0,1,0,0.001,0,
+					'm',-5.85,0,'a',1.8,1.8,0,1,0,0.001,0,
+					'm',-5.85,0,'a',1.8,1.8,0,1,0,0.001,0,
+					'm',0.45,1.35,'l',4.5,-4.5,
+					'm',0.9,0.9,'l',0,1.8,
+				]
+				.join(' ')
+			})
+			.classed('logo', true)
+
 	})
 	
 	// Headers
