d5b519632806c1553ed571273b0ea161e0e814be
Will York <will@ccrc.uga.edu>
2021-10-01 10:52:13 -0400

    Completely removed all static svg files from project

--- build_N-tree.sh
+++ build_N-tree.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-# Usage: ./build_N-tree.sh [svg_source_directory] [clear_flag]
-#  the first argument is the full path to the directory containing GlyTouCan-generated svg image files
-#     the svg directory string must be terminated by a forward slash, e.g., "~/svg/"
-#  if the second argument is set to "clear", previous data for the N-tree will be cleared
+# Usage: ./build_N-tree.sh [clear_flag]
+#  if the firstt argument is set to "clear", previous data for the N-tree will be cleared
 #   Example:
-#              ./build_N-tree.sh ./data/input_svg/ clear
+#              ./build_N-tree.sh clear
 
 start=$(date)
 echo $start
@@ -34,11 +32,6 @@ echo "portal directory is $portalDir"
 echo "Called this program from directory $here" 
 
 for i in "$@"; do
-  ## identify svg_source_directory, as it contains the character '/'
-  if grep -q "\/" <<< $i ; then
-    echo svg source directory is $i
-    svgSource=$i
-  fi
   ## the key word 'clear' is in the argument list
   if [ $i = 'clear' ]; then
     echo "Clearing old glycoTree data files (.txt, csv, .lst, .gTree.svg, .GlycoCT.svg, etc)"
@@ -53,10 +46,6 @@ mkdir ./data/gct/csv
 mkdir ./data/gct/csv/mapped
 mkdir ./data/gct/csv/mapped/sorted
 mkdir ./data/gct/csv/mapped/sorted/pc_annotated
-mkdir ./data/svg
-mkdir ./model/gTree_svg
-mkdir ./model/GlycoCT_svg
-mkdir ./model/json
 mkdir ./model/bak
 
 ## The following is deprecated (at least temporarily)
@@ -66,11 +55,6 @@ mkdir ./model/bak
 
 ## echo Copying extra GlycoCT files
 ## cp ./data/gct/extra/G*.txt ./data/gct/
-
-## The following was replaced with transfer of files from ./data/input_gct
-## echo
-## echo Extracting GlycoCT files from ./data/mammal_N-glycans.json
-## awk -f $codeDir/gctExtract.awk ./data/mammal_N-glycans.json 
 
 echo
 echo "Copying GlycoCt files from $gctIn to $gctDir"
@@ -88,17 +72,8 @@ echo Generating glycoTree csv files
 java -jar $codeDir/GenerateCSV.jar $gctDir/files.lst list 1 &> $logDir/csv.log
 
 echo
-if grep -q "[A-Za-z]" <<< $svgSource ; then
-  echo Fetching svg files from $svgSource
-  $codeDir/fetchSVG.sh $csvDir/ $svgSource $svgDir/ > $logDir/noSVG.lst
-  echo "Accessions lacking SVG support are listed in file ./model/glycansLackingSVG.lst"
-  sed 's/[.]svg//g' $logDir/noSVG.lst | sort > $modelDir/glycansLackingSVG.lst
-fi
-
-echo
 echo "Generating list of glycoTree csv files to process and placing in file $csvDir/files.lst"
 find $csvDir -name "G*.csv" -print -maxdepth 1 | sort > $csvDir/files.lst
-
 
 echo
 echo Fetching current model files
@@ -110,7 +85,7 @@ echo using node file $node_file
 sugar_file=`ls $modelDir/sugars*.csv`
 echo using sugar file $sugar_file
 
-enzyme_file=`ls $modelDir/enzyme-mappings*.csv`
+enzyme_file=`ls $modelDir/enzyme_mappings*.csv`
 echo using enzyme file $enzyme_file
 
 echo formatting node file
@@ -139,18 +114,8 @@ cd $here
 echo
 echo "Generating file containing a list of mapped csv files:   $mappedDir/files.lst"
 find ./data/gct/csv/mapped -name "G*.csv" -print -maxdepth 1  | sort > $mappedDir/files.lst
-echo "Generating a list of svg files that may match the csv files into file $svgDir/hope.lst"
-sed 's/[.]csv/.svg/g' ./data/gct/csv/files.lst > $svgDir/temp.lst
-sed 's/gct\/csv/svg/g' ./data/svg/temp.lst > $svgDir/hope.lst
-rm $svgDir/temp.lst
 
-echo
-echo Flattening svg files
-echo semantic annotation of svg encoding with canonical IDs
-java -jar $codeDir/SVGflatten.jar -l $svgDir/hope.lst -c $mappedDir -a gTree -o $modelDir/gTree_svg -m $modelDir/map_gTree.csv -v 2 -r 1 &> $logDir/gTree_svg.log
-echo semantic annotation of svg encoding with GlycoCT indices
-java -jar $codeDir/SVGflatten.jar -l $svgDir/hope.lst -c $csvDir -a GlycoCT -o $modelDir/GlycoCT_svg -m $modelDir/map_GlycoCT.csv -v 2 -r 1 &> $logDir/GlycoCT_svg.log
-
+## TODO: MAKE THE FOLLOWING WORK WITH NO SVG FILES!!!
 echo
 echo "Combining semantic id (svg/GlycoCT/glycoTree) map files"
 awk -f $codeDir/hashem.awk $modelDir/map_gTree.csv $modelDir/map_GlycoCT.csv > $modelDir/map_both.csv
@@ -160,20 +125,12 @@ echo "Calculating common canonical residues in all accession pairs and writing r
 java -jar $codeDir/CorrelateGlycans.jar -v 2 -l $mappedDir/files.lst -c 512 -j $modelDir/json/match -o $sqlDir/correlation.csv &> ./log/correlate.log
 
 echo
-echo "Replacing svg files in portal"
-find $modelDir/gTree_SVG -name "G*.svg"  -print -maxdepth 1 | xargs -I % cp % $portalDir/svg
-
-echo
-echo "Generating list of accessions associated with svg files and placing in file ./model/svg.lst"
-find $portalDir/svg -maxdepth 1 -name "G*.svg" -print | sort | cut -d \/ -f 7 | awk 'BEGIN {FS=".";} {printf("%s\n", $1);}' > $modelDir/svg.lst
-
-echo
 echo "Generating a list of accessions currently supported by GlycoTree into file $here/accessions.lst"
 find $mappedDir -maxdepth 1 -name "G*.csv" -print | cut -d \/ -f 9 | sort | awk 'BEGIN {FS=".";} {printf("%s\n", $1);}' > $here/accessions.lst
 
 echo
 echo "Generating the home page for the portal as file ./portal/index.html"
-awk -f $codeDir/pIndex.awk $codeDir/pTemplate.txt  $here/accessions.lst > $portalDir/index.html
+awk -f $codeDir/pIndex.awk $codeDir/pTemplate.html  $here/accessions.lst > $portalDir/index.html
 
 echo
 ## The following requires access to SQL DB.  Populate DB first, then use PHP to generate this file (use .tsv format).
--- code/pIndex.awk
+++ code/pIndex.awk
@@ -1,14 +1,17 @@
+## Usage:   awk -f pIndex.awk <template file> <supported accessions file>
+
 BEGIN {
 }
 
+##  the template file is specified by the first argument
 NR == FNR {
   printf("%s\n", $0);
 }
 
+## each line in the file specified by subsequent argument is a supported accession
 NR > FNR {
-  printf("\t  <tr>\n\t    <td>%s</td>\n", $1);
-  printf("\t    <td><a href=\"explore.html?%s\" target=\"_blank\">Explore</a></td></td>\n", $1);
-  printf("\t    <td><a href=\"javascript:showImage('%s');\">View</a></td>\n", $1);
+  printf("\t  <tr>\n");
+  printf("\t    <td><a href=\"javascript:showImage('%s');\">%s</a></td>\n", $1, $1);
   printf("\t  </tr>\n");
 }
 
