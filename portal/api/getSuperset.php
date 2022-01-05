8cd3b33384accedd497968cd6af4f5931fe9d2cc
Will York <will@ccrc.uga.edu>
2022-01-04 19:21:54 -0500

    implemented structure checker (v1); improved modularity of sandbox code

--- portal/api/getSuperset.php
+++ portal/api/getSuperset.php
@@ -9,9 +9,12 @@
 
 include '../config.php';
 include 'bitSet.php';
+include 'glycanCommon.php';
 
 $servername = "";
 $password = "";
+
+
 
 try {
 	if (empty($_GET)) {
@@ -44,100 +47,37 @@ try {
 	}
 	
 	$output = [];
-	$probe_found = 0;
 	$bitData = [];
 	$probe_b64 = "";
-	$bit_query = "SELECT * FROM bitSet"; 
-	$bit_stmt = $connection->prepare($bit_query);
-	$bit_stmt->execute();
-	$bit_result = $bit_stmt->get_result();
-	while ($row = $bit_result->fetch_assoc()) {
-		$bitData[$row['glytoucan_ac']] = $row['base64_composition'];
-		if ($row['glytoucan_ac'] == $acc) {
-			$probe_b64 = $row['base64_composition'];
-			$probe_found = 1;
-		}
-	}
+	fetchBitSetData($connection, $bitData, $probe_b64, $acc);
 	
-	if ($probe_found == 0) {
+	// strlen($probe_b64) is zero if probe is NOT in DB - this may be OK in other php scripts
+	if (strlen($probe_b64) == 0) {
 		$output['glytoucan_ac'] = $acc;
 		$output['error'] = "Accession '" . $acc . "' was not found in the DB";
 		if ($head == "1") header('Content-Type: application/json; charset=utf-8');
 		echo json_encode($output, JSON_PRETTY_PRINT);
 		exit("");
 	}
-	//echo "\n### Fetched " . sizeof($bitData) . " data sets ###";
-	// echo "\n    data set for " . $acc . " is: \n" . $probe_b64;
 	
 	$probe_bs = new BitSet($probe_b64, "base64");
-	$probeFuzzy = checkFuzzy($probe_bs);
-	// echo "\nprobeFuzzy is " . $probeFuzzy;
-	// fuzziness is specified: clear(0) to facilitate logic
-	$probe_bs->clear(0);
-
-	$cp = $probe_bs->cardinality();
 
 	$extended = [];
 	$identical = [];
 	$pruned = [];
 	$extended_fuzzy = [];
-	$record = [];
 	
-	$counter = 0;
-	foreach($bitData as $a => $data) {
-		$target_bs = new BitSet($data, "base64");
-		$targetFuzzy = checkFuzzy($target_bs);
-		// fuzziness is specified: clear(0) to facilitate logic
-		$target_bs->clear(0); 
-		$ct = $target_bs->cardinality();
-		$target_bs->and($probe_bs);
-		$cAnd = $target_bs->cardinality(); // i.e., the number of common bits
-		// cp, ct, and cAnd hold information for classifying targets
-
-		if (!$probeFuzzy) { // process probes that are NOT fuzzy
-			if (!$targetFuzzy) {
-				// neither probe nor target are fuzzy: and logic is easy
-				if ($cAnd == $cp) { // all bits in probe are in target
-					if ($cAnd == $ct) { // all bits in target are in probe
-						// target is identical to probe		
-						array_push($identical, $a);			
-					} else { // all bits in probe are in target but not vice verse
-						// target extends probe
-						array_push($extended, $a);			
-					}
-				} else { // NOT all bits in probe are in target
-					if ($cAnd == $ct) { // all bits in target are in probe
-						// target is pruned version of probe
-						array_push($pruned, $a);
-					}
-				}
-			} else {
-				// only target is fuzzy
-				if ($cAnd == $cp) { // all bits in probe are in target
-					// target is a fuzzy extension of probe	
-					array_push($extended_fuzzy, $a);	
-				}
-			}
-		} else { // process fuzzy probes
-			// can reliably find only non-fuzzy pruned versions of fuzzy probes
-			if (!$targetFuzzy) {
-				// only probe is fuzzy
-				if ($cAnd == $ct) { // all bits in target are in probe
-					// target is a (non-fuzzy) pruned version of fuzzy probe	
-					array_push($pruned, $a);
-				}
-			}
-		}
-	}
+	compareBitSets($bitData, $probe_bs, $identical, $extended, $pruned, $extended_fuzzy);
 	
 	$output['message'] = "some valid results may not be in the DB (e.g., G15407YE [Man-GlcNAc-GlcNAc] or G57321FI [GalNAc])";
 	$output['glytoucan_ac'] = $acc;
-	$output['bit_sets'] = sizeof($bitData);
+	$output['glycans_evaluated'] = sizeof($bitData);
 	$output['identical'] = $identical;
 	$output['extended'] = $extended;
 	$output['extended_fuzzy'] = $extended_fuzzy;
 	$output['pruned'] = $pruned;
 
+	header("Cache-Control: no-cache, must-revalidate");
 	if ($head == "1") header('Content-Type: application/json; charset=utf-8');
 	echo json_encode($output, JSON_PRETTY_PRINT);
 
@@ -148,10 +88,5 @@ try {
 }
 
 $connection->close();
-
-function checkFuzzy($bitset) {
-	$fuzzy = $bitset->get(0) ? true : false;
-	return($fuzzy);
-}
 
 ?>
