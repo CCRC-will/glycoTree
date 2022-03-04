786cd51d0339ecb1ad18a350575a337c9dd447b9
Will York <will@ccrc.uga.edu>
2022-03-04 14:27:54 -0500

    fixed docker issues

--- SQL/populateDB.sh
+++ SQL/populateDB.sh
@@ -145,4 +145,4 @@ $dir/mysqlimport -u $user -p$pw --local --delete -v --fields-terminated-by="\t" 
 
 $dir/mysqlimport -u $user -p$pw --local --delete -v --fields-terminated-by="\t" --ignore-lines=1 --lines-terminated-by="\n" -c instance,rule_id,focus,agent,factor_1,factor_2,taxonomy,curator_id,refs,comment,status,administrator glycotree rule_data.tsv 
 
-$dir/mysqldump -u gt_user -p$pw glycotree > glycotree.sql
+$dir/mysqldump -u gt_user -p$pw glycotree > ./server/glycotree.sql
